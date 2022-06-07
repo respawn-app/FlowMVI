3743f5254a3245fccf523717107374cb6e4fb37b
Nek-12 <vaizin.nikita@gmail.com>
2022-06-07 20:22:06 +0300

    fix comments

--- android-compose/src/main/kotlin/com/nek12/flowMVI/android/compose/MVIComposable.kt
+++ android-compose/src/main/kotlin/com/nek12/flowMVI/android/compose/MVIComposable.kt
@@ -1,7 +1,6 @@
 package com.nek12.flowMVI.android.compose
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.Lifecycle
 import com.nek12.flowMVI.MVIAction
@@ -10,12 +9,28 @@ import com.nek12.flowMVI.MVIProvider
 import com.nek12.flowMVI.MVIState
 import kotlinx.coroutines.Dispatchers
 
-@Composable
 /**
+ * A function that introduces MVIIntentScope to the content and ensures safe lifecycle-aware and efficient collection
+ * of states and actions.
+ * Usage:
+ * ```
+ * @Composable
+ * fun HomeScreen() = MVIComposable(getViewModel<HomeViewModel>()) { state ->
+ *     consume { action ->
+ *         when(action) {
+ *             /*...*/
+ *         }
+ *     }
+ *     when(state) {
+ *         //use state to render content
+ *     }
+ * }
+ * ```
  * @param provider an MVIProvider (usually a viewModel) that handles this screen's logic
  * @param lifecycleState the minimum lifecycle state, in which the activity must be to receive actions/states
- * @param content the actual screen content. Will be recomposed each time you receive a new state
+ * @param content the actual screen content. Will be recomposed each time a new state is received.
  */
+@Composable
 fun <S: MVIState, I: MVIIntent, A: MVIAction, VM: MVIProvider<S, I, A>> MVIComposable(
     provider: VM,
     lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
