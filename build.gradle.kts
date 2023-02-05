d37f8843571c4ada26a6ba3753120907af7b8ece
Nek-12 <vaizin.nikita@gmail.com>
2023-02-05 14:48:02 +0300

    update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,66 @@
+import nl.littlerobots.vcu.plugin.versionCatalogUpdate
+import org.jetbrains.kotlin.konan.file.File.Companion.javaHome
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.detekt)
+    alias(libs.plugins.gradleDoctor)
+    alias(libs.plugins.versions)
     alias(libs.plugins.version.catalog.update)
+    alias(libs.plugins.dokka)
     alias(libs.plugins.atomicfu)
-}
-
-rootProject.group = "com.nek12.flowmvi"
-rootProject.version = "1.0.0-alpha05"
-
-atomicfu {
-    dependenciesVersion = libs.versions.kotlinx.atomicfu.get()
-    transformJvm = true
-    jvmVariant = "VH"
-    transformJs = false
+    alias(libs.plugins.dependencyAnalysis)
 }
 
 buildscript {
     repositories {
         google()
-        gradlePluginPortal()
         mavenCentral()
     }
+
     dependencies {
-        classpath(libs.gradle.versions)
         classpath(libs.android.gradle)
         classpath(libs.kotlin.gradle)
+        classpath(libs.version.gradle)
+        classpath(libs.detekt.gradle)
     }
 }
 
+allprojects {
+    group = Config.artifactId
+    version = Config.versionName
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.dokka.id)
+
+    dependencies {
+        dokkaPlugin(rootProject.libs.dokka.android)
+    }
+}
+
+doctor {
+    javaHome {
+        ensureJavaHomeMatches.set(false)
+    }
+}
+
+dependencyAnalysis {
+    issues {
+        all {
+            ignoreKtx(true)
+        }
+    }
+}
+
+dependencies {
+    detektPlugins(rootProject.libs.detekt.formatting)
+    detektPlugins(rootProject.libs.detekt.compose)
+    detektPlugins(rootProject.libs.detekt.libraries)
+}
+
 versionCatalogUpdate {
-    sortByKey.set(false)
+    sortByKey.set(true)
 
     keep {
         keepUnusedVersions.set(true)
@@ -38,69 +69,37 @@ versionCatalogUpdate {
     }
 }
 
-allprojects {
-    apply(plugin = "com.github.ben-manes.versions")
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    dependencies {
-        // use rootProject as subprojects libs are ambiguous
-        detektPlugins(rootProject.libs.detekt.formatting)
-        detektPlugins(rootProject.libs.detekt.compose)
-    }
-
-    tasks {
-        // needed to generate compose compiler reports. See /scripts
-        withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-            buildUponDefaultConfig = true
-            parallel = true
-            setSource(projectDir)
-            config.setFrom(File(rootDir, Config.Detekt.configFile))
-            basePath = projectDir.absolutePath
-
-            jvmTarget = Config.jvmTarget
-            include(Config.Detekt.includedFiles)
-            exclude(Config.Detekt.excludedFiles)
-            reports {
-                xml.required.set(false)
-                html.required.set(true)
-                txt.required.set(false)
-                sarif.required.set(true)
-                md.required.set(false)
-            }
-        }
-        withType<JavaCompile> {
-            targetCompatibility = "11"
-        }
-        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "11"
-                freeCompilerArgs = freeCompilerArgs + Config.kotlinCompilerArgs
-            }
-        }
-        withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>().configureEach {
-            // outputFormatter = "json"
-
-            fun stabilityLevel(version: String): Int {
-                Config.stabilityLevels.forEachIndexed { index, postfix ->
-                    val regex = ".*[.\\-]$postfix[.\\-\\d]*".toRegex(RegexOption.IGNORE_CASE)
-                    if (version.matches(regex)) return index
-                }
-                return Config.stabilityLevels.size
-            }
-
-            rejectVersionIf {
-                stabilityLevel(currentVersion) > stabilityLevel(candidate.version)
-            }
-        }
-    }
+atomicfu {
+    dependenciesVersion = libs.versions.kotlinx.atomicfu.get()
+    transformJvm = true
+    jvmVariant = "VH"
+    transformJs = false
 }
 
-subprojects {
-    group = rootProject.group
-    version = rootProject.version
+tasks.dokkaHtmlMultiModule.configure {
+    outputDirectory.set(buildDir.resolve("dokka"))
 }
 
 tasks {
+    // needed to generate compose compiler reports. See /scripts
+    withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        buildUponDefaultConfig = true
+        parallel = true
+        setSource(projectDir)
+        config.setFrom(File(rootDir, Config.Detekt.configFile))
+        basePath = projectDir.absolutePath
+        jvmTarget = Config.jvmTarget.target
+        include(Config.Detekt.includedFiles)
+        exclude(Config.Detekt.excludedFiles)
+        reports {
+            xml.required.set(false)
+            html.required.set(true)
+            txt.required.set(false)
+            sarif.required.set(true)
+            md.required.set(false)
+        }
+    }
+
     register<io.gitlab.arturbosch.detekt.Detekt>("detektFormat") {
         description = "Formats whole project."
         autoCorrect = true
@@ -109,5 +108,21 @@ tasks {
     register<io.gitlab.arturbosch.detekt.Detekt>("detektAll") {
         description = "Run detekt on whole project"
         autoCorrect = false
+    }
+
+    withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>().configureEach {
+        outputFormatter = "json"
+
+        fun stabilityLevel(version: String): Int {
+            Config.stabilityLevels.forEachIndexed { index, postfix ->
+                val regex = ".*[.\\-]$postfix[.\\-\\d]*".toRegex(RegexOption.IGNORE_CASE)
+                if (version.matches(regex)) return index
+            }
+            return Config.stabilityLevels.size
+        }
+
+        rejectVersionIf {
+            stabilityLevel(currentVersion) > stabilityLevel(candidate.version)
+        }
     }
 }
