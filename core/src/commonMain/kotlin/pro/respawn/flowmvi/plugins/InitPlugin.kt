6a9f613f419bd593e4eb0167d11d7f0232f39568
Nek-12 <vaizin.nikita@gmail.com>
2023-07-30 15:06:29 +0300

    add init plugin

---
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/plugins/InitPlugin.kt
@@ -0,0 +1,22 @@
+package pro.respawn.flowmvi.plugins
+
+import pro.respawn.flowmvi.api.FlowMVIDSL
+import pro.respawn.flowmvi.api.MVIAction
+import pro.respawn.flowmvi.api.MVIIntent
+import pro.respawn.flowmvi.api.MVIState
+import pro.respawn.flowmvi.api.PipelineContext
+import pro.respawn.flowmvi.api.StorePlugin
+import pro.respawn.flowmvi.dsl.StoreBuilder
+import pro.respawn.flowmvi.dsl.storePlugin
+
+@FlowMVIDSL
+public fun <S : MVIState, I : MVIIntent, A : MVIAction> StoreBuilder<S, I, A>.init(
+    block: suspend PipelineContext<S, I, A>.() -> Unit
+): Unit = install(initPlugin(block))
+
+@FlowMVIDSL
+public fun <S : MVIState, I : MVIIntent, A : MVIAction> initPlugin(
+    block: suspend PipelineContext<S, I, A>.() -> Unit,
+): StorePlugin<S, I, A> = storePlugin {
+    onStart(block)
+}
