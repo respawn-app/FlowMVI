a140dd9787227c92bc7def86c9b0d38a045ca8e2
Nek-12 <vaizin.nikita@gmail.com>
2023-07-30 15:50:48 +0300

    made abstract store plugins props final

--- core/src/commonMain/kotlin/pro/respawn/flowmvi/plugins/AbstractStorePlugin.kt
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/plugins/AbstractStorePlugin.kt
@@ -6,12 +6,12 @@ import pro.respawn.flowmvi.api.MVIState
 import pro.respawn.flowmvi.api.StorePlugin
 
 public abstract class AbstractStorePlugin<S : MVIState, I : MVIIntent, A : MVIAction>(
-    override val name: String? = null,
+    final override val name: String? = null,
 ) : StorePlugin<S, I, A> {
 
-    override fun toString(): String = "StorePlugin \"${name ?: super.toString()}\""
-    override fun hashCode(): Int = name?.hashCode() ?: super.hashCode()
-    override fun equals(other: Any?): Boolean {
+    final override fun toString(): String = "StorePlugin \"${name ?: super.toString()}\""
+    final override fun hashCode(): Int = name?.hashCode() ?: super.hashCode()
+    final override fun equals(other: Any?): Boolean {
         if (other !is StorePlugin<*, *, *>) return false
         if (other.name == null && name == null) return this === other
         return name == other.name
