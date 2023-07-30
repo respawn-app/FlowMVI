147ecfffc97c7c083bd7c31cd91aa98230b8f3a6
Nek-12 <vaizin.nikita@gmail.com>
2023-07-30 17:59:07 +0300

    docs for the store builder

--- core/src/commonMain/kotlin/pro/respawn/flowmvi/dsl/StoreBuilder.kt
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/dsl/StoreBuilder.kt
@@ -25,7 +25,7 @@ import kotlin.jvm.JvmName
  * @see [Store.initial]
  */
 @JvmInline
-public value class Init<S : MVIState>(public val state: S)
+public value class Init<S : MVIState> internal constructor(public val state: S)
 
 public typealias BuildStore<S, I, A> = StoreBuilder<S, I, A>.() -> Init<S>
 
@@ -43,6 +43,8 @@ public class StoreBuilder<S : MVIState, I : MVIIntent, A : MVIAction> @Published
     /**
      * Set the future name of the store.
      * See [Store.name] for more info.
+     *
+     * null by default
      */
     @FlowMVIDSL
     public var name: String? = null
@@ -50,30 +52,67 @@ public class StoreBuilder<S : MVIState, I : MVIIntent, A : MVIAction> @Published
     /**
      * Declare that intents must be processed in parallel.
      * All guarantees on the order of [MVIIntent]s will be lost.
-     * Intents may still be dropped according to [onOverflow]
+     * Intents may still be dropped according to [onOverflow].
+     * Intents are not **obtained** in parallel, just processed.
+     *
+     * false by default.
      */
     @FlowMVIDSL
     public var parallelIntents: Boolean = false
 
+    /**
+     * Provide the [ActionShareBehavior] for the store.
+     * For stores where actions are of type [Nothing] this must be set to [ActionShareBehavior.Disabled].
+     * Will be set automatically when using the two-argument store builder.
+     *
+     * [ActionShareBehavior.Distribute] by default.
+     */
     @FlowMVIDSL
     public var actionShareBehavior: ActionShareBehavior = ActionShareBehavior.Distribute()
 
+    /**
+     * Designate behavior for when [pro.respawn.flowmvi.api.IntentReceiver]'s [MVIIntent] pool overflows
+     * [BufferOverflow.DROP_OLDEST] by default
+     */
     @FlowMVIDSL
     public var onOverflow: BufferOverflow = BufferOverflow.DROP_OLDEST
 
+    /**
+     * Designate the maximum capacity of [MVIIntent]s waiting for processing
+     * in the [pro.respawn.flowmvi.api.IntentReceiver]'s queue.
+     * Intents that overflow this capacity will be processed according to [onOverflow].
+     */
     @FlowMVIDSL
     public var intentCapacity: Int = Channel.UNLIMITED
 
+    /**
+     * Install an existing [StorePlugin]. See the other overload to build the plugin on the fly.
+     * This installs a prebuilt plugin.
+     * Plugins will **preserve** the order of installation and will proceed according to this order.
+     * See [StorePlugin] for comprehensive information on the behavior of plugins.
+     * Installation of the same plugin multiple times is **not allowed**.
+     * See [StorePlugin.name] for more info and solutions.
+     */
     @FlowMVIDSL
     public fun install(plugin: StorePlugin<S, I, A>) {
         require(plugins.add(plugin)) { duplicatePluginMessage(plugin.toString()) }
     }
 
+    /**
+     * Install a new [StorePlugin]. See the other overload to build the plugin on the fly.
+     * This installs a prebuilt plugin.
+     * Plugins will **preserve** the order of installation and will proceed according to this order.
+     * See [StorePlugin] for comprehensive information on the behavior of plugins.
+     */
     @FlowMVIDSL
     public fun install(
         block: StorePluginBuilder<S, I, A>.() -> Unit
     ): Unit = install(storePlugin(block))
 
+    /**
+     * Create and return an [Init] holder with initial values for the store.
+     * This must be **the last statement** in this store builder's block.
+     */
     @FlowMVIDSL
     public fun initial(state: S): Init<S> = Init(state)
 
@@ -90,6 +129,10 @@ public class StoreBuilder<S : MVIState, I : MVIIntent, A : MVIAction> @Published
     ).let(::StoreImpl)
 }
 
+/**
+ * Build a new [Store] using [StoreBuilder].
+ * The store is not launched and created eagerly, with all its plugins.
+ */
 @FlowMVIDSL
 public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> store(
     @BuilderInference crossinline configure: BuildStore<S, I, A>,
@@ -97,20 +140,36 @@ public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> store(
     build(configure())
 }
 
+/**
+ * * Build a new [Store] using [StoreBuilder] but disallow using [MVIAction]s.
+ * The store is **not** launched and created eagerly, with all its plugins.
+ *
+ * If your code doesn't compile, you are looking for another overload with three type parameters, i.e:
+ * `store<_, _, _>()`
+ */
 @FlowMVIDSL
 @JvmName("noActionStore")
 public inline fun <S : MVIState, I : MVIIntent> store(
     @BuilderInference crossinline configure: BuildStore<S, I, Nothing>,
-): Store<S, I, Nothing> = store<S, I, Nothing> {
+): Store<S, I, Nothing> = StoreBuilder<S, I, Nothing>().run {
+    val init = configure()
     actionShareBehavior = ActionShareBehavior.Disabled
-    configure()
+    build(init)
 }
 
+/**
+ * Build a new [Store] using [StoreBuilder].
+ * The store is **not** launched and created lazily, with all its plugins.
+ */
 @FlowMVIDSL
 public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> lazyStore(
     @BuilderInference crossinline configure: BuildStore<S, I, A>,
 ): Lazy<Store<S, I, A>> = lazy { store(configure) }
 
+/**
+ * Build a new [Store] using [StoreBuilder].
+ * The store is built **lazily** and launched on **first access** (i.e. after bulding).
+ */
 @FlowMVIDSL
 public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> lazyStore(
     scope: CoroutineScope,
@@ -123,4 +182,5 @@ private fun duplicatePluginMessage(name: String) = """
     You either have installed the same plugin instance twice or have installed two plugins with the same name.
     To fix, please either create a new plugin instance for each installation (when not using names) 
     or override the plugin name to be unique among all plugins for this store.
+    Consult the StorePlugin docs to learn more
 """.trimIndent()
