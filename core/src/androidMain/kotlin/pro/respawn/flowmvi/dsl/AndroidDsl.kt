35e91d3963c47616001470d5f728637ed9fb5d9a
Nek-12 <vaizin.nikita@gmail.com>
2023-10-16 20:41:37 +0300

    add dsl annotations to context receiver functions

--- core/src/androidMain/kotlin/pro/respawn/flowmvi/dsl/AndroidDsl.kt
+++ core/src/androidMain/kotlin/pro/respawn/flowmvi/dsl/AndroidDsl.kt
@@ -1,6 +1,7 @@
 package pro.respawn.flowmvi.dsl
 
 import pro.respawn.flowmvi.api.ActionReceiver
+import pro.respawn.flowmvi.api.FlowMVIDSL
 import pro.respawn.flowmvi.api.IntentReceiver
 import pro.respawn.flowmvi.api.MVIAction
 import pro.respawn.flowmvi.api.MVIIntent
@@ -8,23 +9,23 @@ import pro.respawn.flowmvi.api.MVIIntent
 /**
  * An alias for [IntentReceiver.send]
  */
-context(IntentReceiver<I>)
+context(IntentReceiver<I>) @FlowMVIDSL
 public fun <I : MVIIntent> I.send(): Unit = intent(this)
 
 /**
  * An alias for [ActionReceiver.action]
  */
-context(ActionReceiver<A>)
+context(ActionReceiver<A>) @FlowMVIDSL
 public suspend fun <A : MVIAction> A.send(): Unit = action(this)
 
 /**
  * An alias for [IntentReceiver.emit]
  */
-context(IntentReceiver<I>)
+context(IntentReceiver<I>) @FlowMVIDSL
 public suspend fun <I : MVIIntent> I.emit(): Unit = emit(this)
 
 /**
  * An alias for [ActionReceiver.action]
  */
-context(ActionReceiver<A>)
+context(ActionReceiver<A>) @FlowMVIDSL
 public suspend fun <A : MVIAction> A.emit(): Unit = emit(this)
