3a0346f8e398efa9ce7ec426154881e736650958
Nek-12 <vaizin.nikita@gmail.com>
2023-10-16 18:54:07 +0300

    add context receiver functions for android dsl

---
+++ core/src/androidMain/kotlin/pro/respawn/flowmvi/dsl/AndroidDsl.kt
@@ -0,0 +1,30 @@
+package pro.respawn.flowmvi.dsl
+
+import pro.respawn.flowmvi.api.ActionReceiver
+import pro.respawn.flowmvi.api.IntentReceiver
+import pro.respawn.flowmvi.api.MVIAction
+import pro.respawn.flowmvi.api.MVIIntent
+
+/**
+ * An alias for [IntentReceiver.send]
+ */
+context(IntentReceiver<I>)
+public fun <I : MVIIntent> I.send(): Unit = intent(this)
+
+/**
+ * An alias for [ActionReceiver.action]
+ */
+context(ActionReceiver<A>)
+public suspend fun <A : MVIAction> A.send(): Unit = action(this)
+
+/**
+ * An alias for [IntentReceiver.emit]
+ */
+context(IntentReceiver<I>)
+public suspend fun <I : MVIIntent> I.emit(): Unit = emit(this)
+
+/**
+ * An alias for [ActionReceiver.action]
+ */
+context(ActionReceiver<A>)
+public suspend fun <A : MVIAction> A.emit(): Unit = emit(this)
