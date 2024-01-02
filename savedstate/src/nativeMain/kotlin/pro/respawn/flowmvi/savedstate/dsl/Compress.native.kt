a42f22f195786b9b8f740552074f1f9177215d63
Nek-12 <vaizin.nikita@gmail.com>
2024-01-03 00:58:21 +0300

    implement JVM/android file compression via gzip

---
+++ savedstate/src/nativeMain/kotlin/pro/respawn/flowmvi/savedstate/dsl/Compress.native.kt
@@ -0,0 +1,6 @@
+package pro.respawn.flowmvi.savedstate.dsl
+
+import kotlinx.io.files.Path
+
+internal actual suspend fun writeCompressed(data: String, to: Path) = write(data, to)
+internal actual suspend fun readCompressed(from: Path): String? = read(from)
