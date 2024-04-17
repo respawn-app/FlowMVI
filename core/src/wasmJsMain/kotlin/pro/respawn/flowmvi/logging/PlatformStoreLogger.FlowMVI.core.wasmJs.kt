3e152df4389f05a9d161fb798ccd874f1635e0d5
Nek-12 <vaizin.nikita@gmail.com>
2024-04-17 14:38:54 +0300

    fix logging on wasm

--- core/src/wasmJsMain/kotlin/pro/respawn/flowmvi/logging/PlatformStoreLogger.FlowMVI.core.wasmJs.kt
+++ core/src/wasmJsMain/kotlin/pro/respawn/flowmvi/logging/PlatformStoreLogger.FlowMVI.core.wasmJs.kt
@@ -1,19 +1,19 @@
-@file:Suppress("TrimMultilineRawString", "UnusedParameter")
+@file:Suppress("UnusedParameter")
 package pro.respawn.flowmvi.logging
 
-private fun log(message: String): Unit = js("""{ console.log(message); }""")
+private fun log(message: String): Unit = js("""console.log(message)""")
 
-private fun info(message: String): Unit = js("""{ console.info(message); }""")
+private fun info(message: String): Unit = js("""console.info(message)""")
 
-private fun warn(message: String): Unit = js("""{ console.warn(message); }""")
+private fun warn(message: String): Unit = js("""console.warn(message)""")
 
-private fun error(message: String): Unit = js("""{ console.error(message); }""")
+private fun error(message: String): Unit = js("""console.error(message)""")
 
 /**
  * A [StoreLogger] instance for each supported platform
  */
 public actual val PlatformStoreLogger: StoreLogger = StoreLogger { level, tag, message ->
-    val template = "${if (tag == null) "" else "$tag: "}${message()}()"
+    val template = "${if (tag == null) "" else "$tag: "}${message()}"
     when (level) {
         StoreLogLevel.Trace, StoreLogLevel.Debug -> log(template)
         StoreLogLevel.Info -> info(template)
