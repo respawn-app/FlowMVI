521354935eae82982ac53a799c0ccb6d05c400e5
Nek-12 <vaizin.nikita@gmail.com>
2024-04-22 19:56:12 +0300

    add some more inlines to state update functions

--- core/src/commonMain/kotlin/pro/respawn/flowmvi/util/ReentrantLock.kt
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/util/ReentrantLock.kt
@@ -7,7 +7,9 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.coroutineContext
 import kotlin.jvm.JvmInline
 
-internal suspend inline fun <T> Mutex.withReentrantLock(crossinline block: suspend () -> T): T {
+@PublishedApi
+internal suspend inline fun <T> Mutex?.withReentrantLock(crossinline block: suspend () -> T): T {
+    if (this == null) return block()
     val key = ReentrantMutexContextKey(this)
     // call block directly when this mutex is already locked in the context
     if (coroutineContext[key] != null) return block()
@@ -18,11 +20,13 @@ internal suspend inline fun <T> Mutex.withReentrantLock(crossinline block: suspe
 }
 
 @JvmInline
+@PublishedApi
 internal value class ReentrantMutexContextElement(
     override val key: ReentrantMutexContextKey
 ) : CoroutineContext.Element
 
 @JvmInline
+@PublishedApi
 internal value class ReentrantMutexContextKey(
     val mutex: Mutex
 ) : CoroutineContext.Key<ReentrantMutexContextElement>
