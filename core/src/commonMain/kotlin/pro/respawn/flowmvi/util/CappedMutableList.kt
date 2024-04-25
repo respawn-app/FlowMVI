6adf550222fd04d44f59a9d904c1fcc5bbaa653a
Nek-12 <vaizin.nikita@gmail.com>
2024-04-25 15:39:43 +0300

    remove synchronization from capped mutable list as locks are used now

--- core/src/commonMain/kotlin/pro/respawn/flowmvi/util/CappedMutableList.kt
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/util/CappedMutableList.kt
@@ -1,37 +1,35 @@
 package pro.respawn.flowmvi.util
 
-import kotlinx.atomicfu.locks.SynchronizedObject
-import kotlinx.atomicfu.locks.synchronized
-
 internal class CappedMutableList<T>(
     private val maxSize: Int,
     private val backing: MutableList<T> = mutableListOf(),
-) : MutableList<T> by backing, SynchronizedObject() {
+) : MutableList<T> by backing {
+
 
     init {
         require(maxSize > 0) { "The max size of the collection must be >0, got $maxSize" }
     }
 
-    override fun add(element: T): Boolean = synchronized(this) {
+    override fun add(element: T): Boolean {
         backing.add(element)
         removeOverflowing()
         return true
     }
 
-    override fun add(index: Int, element: T) = synchronized(this) {
+    override fun add(index: Int, element: T) {
         require(index <= maxSize)
         backing.add(index, element)
         removeOverflowing()
     }
 
-    override fun addAll(index: Int, elements: Collection<T>): Boolean = synchronized(this) {
+    override fun addAll(index: Int, elements: Collection<T>): Boolean {
         require(index <= maxSize)
         val added = backing.addAll(index, elements)
         removeOverflowing()
         return added
     }
 
-    override fun addAll(elements: Collection<T>): Boolean = synchronized(this) {
+    override fun addAll(elements: Collection<T>): Boolean {
         val added = backing.addAll(elements)
         removeOverflowing()
         return added
