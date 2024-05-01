971e3e0e5fe1d4c3f80f32f7b062040ba6cc773a
Nek-12 <vaizin.nikita@gmail.com>
2024-05-01 16:34:32 +0300

    fix lint

--- savedstate/src/wasmJsMain/kotlin/pro/respawn/flowmvi/savedstate/platform/FileAccess.wasmJs.kt
+++ savedstate/src/wasmJsMain/kotlin/pro/respawn/flowmvi/savedstate/platform/FileAccess.wasmJs.kt
@@ -7,7 +7,8 @@ internal actual suspend fun writeCompressed(data: String?, path: String) = write
 internal actual suspend fun readCompressed(path: String): String? = read(path)
 
 internal actual suspend fun write(
-    data: String?, path: String
+    data: String?,
+    path: String
 ) = if (data != null) localStorage.setItem(path, data) else localStorage.removeItem(path)
 
 internal actual suspend fun read(path: String): String? = localStorage.getItem(path)
