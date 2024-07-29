13afcb64566a2072adca2e7e1072445dbe74ebca
Nek-12 <vaizin.nikita@gmail.com>
2024-07-29 21:05:19 +0200

    breaking: deprecate functions that used context receivers as they are deprecated themselves

--- core/src/jvmMain/kotlin/pro/respawn/flowmvi/dsl/JvmDsl.kt
+++ core/src/jvmMain/kotlin/pro/respawn/flowmvi/dsl/JvmDsl.kt
@@ -10,22 +10,26 @@ import pro.respawn.flowmvi.api.MVIIntent
  * An alias for [IntentReceiver.send]
  */
 context(IntentReceiver<I>) @FlowMVIDSL
+@Deprecated("Context receivers are deprecated in Kotlin", ReplaceWith("intent(this)"))
 public fun <I : MVIIntent> I.send(): Unit = intent(this)
 
 /**
  * An alias for [ActionReceiver.action]
  */
 context(ActionReceiver<A>) @FlowMVIDSL
+@Deprecated("Context receivers are deprecated in Kotlin", ReplaceWith("action(this)"))
 public suspend fun <A : MVIAction> A.send(): Unit = action(this)
 
 /**
  * An alias for [IntentReceiver.emit]
  */
 context(IntentReceiver<I>) @FlowMVIDSL
+@Deprecated("Context receivers are deprecated in Kotlin", ReplaceWith("emit(this)"))
 public suspend fun <I : MVIIntent> I.emit(): Unit = emit(this)
 
 /**
  * An alias for [ActionReceiver.action]
  */
 context(ActionReceiver<A>) @FlowMVIDSL
+@Deprecated("Context receivers are deprecated in Kotlin", ReplaceWith("emit(this)"))
 public suspend fun <A : MVIAction> A.emit(): Unit = emit(this)
