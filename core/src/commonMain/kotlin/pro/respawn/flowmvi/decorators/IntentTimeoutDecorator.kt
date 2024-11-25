feb8bdfa20f032fa85800d645318c56cb11a096c
Nek-12 <vaizin.nikita@gmail.com>
2024-11-25 17:10:42 +0100

    feat: add Intent Timeout decorator

---
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/decorators/IntentTimeoutDecorator.kt
@@ -0,0 +1,34 @@
+package pro.respawn.flowmvi.decorators
+
+import kotlinx.coroutines.withTimeoutOrNull
+import pro.respawn.flowmvi.annotation.ExperimentalFlowMVIAPI
+import pro.respawn.flowmvi.api.FlowMVIDSL
+import pro.respawn.flowmvi.api.MVIAction
+import pro.respawn.flowmvi.api.MVIIntent
+import pro.respawn.flowmvi.api.MVIState
+import pro.respawn.flowmvi.api.PipelineContext
+import pro.respawn.flowmvi.decorator.StoreDecorator
+import pro.respawn.flowmvi.decorator.decorator
+import pro.respawn.flowmvi.dsl.StoreBuilder
+import kotlin.time.Duration
+
+@ExperimentalFlowMVIAPI
+@FlowMVIDSL
+public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> intentTimeoutDecorator(
+    timeout: Duration,
+    crossinline onTimeout: suspend PipelineContext<S, I, A>.(attempted: I) -> I? = { null },
+): StoreDecorator<S, I, A> = decorator {
+    onIntent {
+        withTimeoutOrNull(timeout) {
+            // can also return null so exit early to not confuse the 2 outcomes
+            return@withTimeoutOrNull proceed(it)
+        } ?: onTimeout(it)
+    }
+}
+
+@ExperimentalFlowMVIAPI
+@FlowMVIDSL
+public inline fun <S : MVIState, I : MVIIntent, A : MVIAction> StoreBuilder<S, I, A>.timeoutIntents(
+    timeout: Duration,
+    crossinline onTimeout: suspend PipelineContext<S, I, A>.(attempted: I) -> I? = { null },
+): Unit = install(intentTimeoutDecorator(timeout, onTimeout))
