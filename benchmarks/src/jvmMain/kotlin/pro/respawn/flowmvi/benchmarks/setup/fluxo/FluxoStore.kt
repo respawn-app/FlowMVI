09b3dc73a57ebae15db9512a94e2a7c96cdcbb9d
Nek-12 <vaizin.nikita@gmail.com>
2024-11-29 12:11:27 +0100

    use better optimized fluxo store config for benchmarks

--- benchmarks/src/jvmMain/kotlin/pro/respawn/flowmvi/benchmarks/setup/fluxo/FluxoStore.kt
+++ benchmarks/src/jvmMain/kotlin/pro/respawn/flowmvi/benchmarks/setup/fluxo/FluxoStore.kt
@@ -3,14 +3,15 @@ package pro.respawn.flowmvi.benchmarks.setup.fluxo
 import kotlinx.coroutines.Dispatchers
 import kt.fluxo.core.annotation.ExperimentalFluxoApi
 import kt.fluxo.core.store
-import kt.fluxo.core.updateState
 import pro.respawn.flowmvi.benchmarks.setup.BenchmarkIntent
 import pro.respawn.flowmvi.benchmarks.setup.BenchmarkState
 
 @OptIn(ExperimentalFluxoApi::class)
 internal inline fun fluxoStore(
-) = store(BenchmarkState(), handler = { it: BenchmarkIntent ->
-    updateState { it.copy(counter = it.counter + 1) }
+) = store(BenchmarkState(), reducer = { it: BenchmarkIntent ->
+    when (it) {
+        BenchmarkIntent.Increment -> copy(counter = counter + 1)
+    }
 }) {
     coroutineContext = Dispatchers.Unconfined
     intentStrategy = Direct
