6a29cdda6d6668d2291118c1636047841a784666
Nek-12 <vaizin.nikita@gmail.com>
2024-12-08 12:37:27 +0100

    feat: add reset state plugin

---
+++ core/src/commonMain/kotlin/pro/respawn/flowmvi/plugins/ResetStatePlugin.kt
@@ -0,0 +1,35 @@
+package pro.respawn.flowmvi.plugins
+
+import pro.respawn.flowmvi.api.MVIAction
+import pro.respawn.flowmvi.api.MVIIntent
+import pro.respawn.flowmvi.api.MVIState
+import pro.respawn.flowmvi.api.Store
+import pro.respawn.flowmvi.api.StorePlugin
+import pro.respawn.flowmvi.dsl.StoreBuilder
+import pro.respawn.flowmvi.dsl.plugin
+import pro.respawn.flowmvi.dsl.updateStateImmediate
+
+private const val Name = "ResetStatePlugin"
+
+/**
+ * Normally, [Store]s do not reset their state back to the initial value when they are stopped.
+ *
+ * This plugin sets the state back to the initial value (defined in the builder) each time the store is stopped.
+ *
+ * Install this plugin first preferably.
+ *
+ * **This plugin can only be installed ONCE**.
+ **/
+public fun <S : MVIState, I : MVIIntent, A : MVIAction> resetStatePlugin(): StorePlugin<S, I, A> = plugin<S, I, A> {
+    this.name = Name
+    onStop { e ->
+        updateStateImmediate { config.initial }
+    }
+}
+
+/**
+ * Install a new [resetStatePlugin].
+ **/
+public fun <S : MVIState, I : MVIIntent, A : MVIAction> StoreBuilder<S, I, A>.resetStateOnStop() = install(
+    resetStatePlugin()
+)
