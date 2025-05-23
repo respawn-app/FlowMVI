e2cc79e4f820e04c7003b529153b6f932523f5e0
Nek-12 <vaizin.nikita@gmail.com>
2025-05-23 16:05:28 +0200

    feat: update live templates to allow generating plugins for a specific store

--- sample/src/commonMain/kotlin/pro/respawn/flowmvi/sample/features/progressive/ProgressiveScreen.kt
+++ sample/src/commonMain/kotlin/pro/respawn/flowmvi/sample/features/progressive/ProgressiveScreen.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
