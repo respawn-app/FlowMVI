3102213b772287248c7b6ba9644c3bca5c34e6e6
Nek-12 <vaizin.nikita@gmail.com>
2025-09-16 13:23:21 +0200

    fix lint

--- debugger/server/src/commonMain/kotlin/pro/respawn/flowmvi/debugger/server/navigation/util/NavAnimations.kt
+++ debugger/server/src/commonMain/kotlin/pro/respawn/flowmvi/debugger/server/navigation/util/NavAnimations.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.androidPredictiveBackAnimatable
 import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.androidPredictiveBackAnimatableV2
 import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.predictiveBackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
