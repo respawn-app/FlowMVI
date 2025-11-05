311bcaa7ab61d88396589fe7b3e03dd7ae18bcd4
Nek-12 <vaizin.nikita@gmail.com>
2025-11-05 19:49:47 +0100

    chore: trim redundant tests

--- savedstate/src/jvmTest/kotlin/pro/respawn/flowmvi/savedstate/dsl/LoggingSaverTest.kt
+++ savedstate/src/jvmTest/kotlin/pro/respawn/flowmvi/savedstate/dsl/LoggingSaverTest.kt
@@ -8,160 +8,35 @@ import pro.respawn.flowmvi.logging.StoreLogger
 class LoggingSaverTest : FreeSpec({
 
     "Given a LoggingSaver" - {
+        "when logging around delegate operations" - {
+            "then it should pass data to delegate and emit logs with configured level and tag" {
+                val savedStates = mutableListOf<String?>()
+                val observedLevels = mutableListOf<StoreLogLevel>()
+                val observedTags = mutableListOf<String?>()
 
-        "when the delegate saver works normally" - {
-            "then save and restore should work and logging should occur" {
-                val testState = "test-state"
-                var savedState: String? = null
-                val logMessages = mutableListOf<String>()
-                var logLevel: StoreLogLevel? = null
-                var logTag: String? = null
-
-                val mockLogger = StoreLogger { level, tag, message ->
-                    logLevel = level
-                    logTag = tag
-                    logMessages.add(message())
+                val logger = StoreLogger { level, tag, _ ->
+                    observedLevels += level
+                    observedTags += tag
                 }
 
-                val delegateSaver = Saver(
-                    save = { savedState = it },
-                    restore = { savedState }
+                val delegate = Saver(
+                    save = { savedStates += it },
+                    restore = { savedStates.lastOrNull() }
                 )
 
-                val loggingSaver = LoggingSaver(
-                    delegate = delegateSaver,
-                    logger = mockLogger,
-                    level = StoreLogLevel.Debug,
+                val saver = LoggingSaver(
+                    delegate = delegate,
+                    logger = logger,
+                    level = StoreLogLevel.Info,
                     tag = "TestSaver"
                 )
 
-                loggingSaver.save(testState)
-                savedState shouldBe testState
-                logMessages.size shouldBe 1
-                logMessages[0] shouldBe "Saving state: $testState"
-                logLevel shouldBe StoreLogLevel.Debug
-                logTag shouldBe "TestSaver"
+                saver.save("value")
+                saver.restore()
 
-                logMessages.clear()
-                val result = loggingSaver.restore()
-                result shouldBe testState
-                logMessages.size shouldBe 1
-                logMessages[0] shouldBe "Restored state: $testState"
-                logLevel shouldBe StoreLogLevel.Debug
-                logTag shouldBe "TestSaver"
-            }
-        }
-
-        "when saving null state" - {
-            "then it should log null state" {
-                var savedState: String? = null
-                val logMessages = mutableListOf<String>()
-
-                val mockLogger = StoreLogger { _, _, message ->
-                    logMessages.add(message())
-                }
-
-                val delegateSaver = Saver(
-                    save = { savedState = it },
-                    restore = { savedState }
-                )
-
-                val loggingSaver = LoggingSaver(
-                    delegate = delegateSaver,
-                    logger = mockLogger
-                )
-
-                loggingSaver.save(null)
-                savedState shouldBe null
-                logMessages.size shouldBe 1
-                logMessages[0] shouldBe "Saving state: null"
-            }
-        }
-
-        "when restoring null state" - {
-            "then it should log null state" {
-                val logMessages = mutableListOf<String>()
-
-                val mockLogger = StoreLogger { _, _, message ->
-                    logMessages.add(message())
-                }
-
-                val delegateSaver = Saver<String>(
-                    save = { },
-                    restore = { null }
-                )
-
-                val loggingSaver = LoggingSaver(
-                    delegate = delegateSaver,
-                    logger = mockLogger
-                )
-
-                val result = loggingSaver.restore()
-                result shouldBe null
-                logMessages.size shouldBe 1
-                logMessages[0] shouldBe "Restored state: null"
-            }
-        }
-
-        "when using default parameters" - {
-            "then it should use default log level and tag" {
-                val testState = "test-state"
-                var savedState: String? = null
-                var logLevel: StoreLogLevel? = null
-                var logTag: String? = null
-
-                val mockLogger = StoreLogger { level, tag, _ ->
-                    logLevel = level
-                    logTag = tag
-                }
-
-                val delegateSaver = Saver(
-                    save = { savedState = it },
-                    restore = { savedState }
-                )
-
-                val loggingSaver = LoggingSaver(
-                    delegate = delegateSaver,
-                    logger = mockLogger
-                )
-
-                loggingSaver.save(testState)
-                logLevel shouldBe StoreLogLevel.Trace
-                logTag shouldBe "Saver"
-            }
-        }
-
-        "when using custom log level" - {
-            "then it should use the specified level for normal operations" {
-                val testState = "test-state"
-                var savedState: String? = null
-                var saveLogLevel: StoreLogLevel? = null
-                var restoreLogLevel: StoreLogLevel? = null
-
-                val mockLogger = StoreLogger { level, _, _ ->
-                    if (saveLogLevel == null) {
-                        saveLogLevel = level
-                    } else {
-                        restoreLogLevel = level
-                    }
-                }
-
-                val delegateSaver = Saver(
-                    save = { savedState = it },
-                    restore = { savedState }
-                )
-
-                val loggingSaver = LoggingSaver(
-                    delegate = delegateSaver,
-                    logger = mockLogger,
-                    level = StoreLogLevel.Warn
-                )
-
-                loggingSaver.save(testState)
-                saveLogLevel shouldBe StoreLogLevel.Warn
-
-                loggingSaver.restore()
-                restoreLogLevel shouldBe StoreLogLevel.Warn
+                savedStates shouldBe listOf("value")
+                observedLevels shouldBe listOf(StoreLogLevel.Info, StoreLogLevel.Info)
+                observedTags shouldBe listOf("TestSaver", "TestSaver")
             }
         }
     }
--- savedstate/src/jvmTest/kotlin/pro/respawn/flowmvi/savedstate/dsl/NoOpSaverTest.kt
+++ savedstate/src/jvmTest/kotlin/pro/respawn/flowmvi/savedstate/dsl/NoOpSaverTest.kt
@@ -2,35 +2,10 @@ package pro.respawn.flowmvi.savedstate.dsl
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 
 class NoOpSaverTest : FreeSpec({
 
     "Given a NoOpSaver" - {
-
-        "when creating instances for different types" - {
-            "then each instance should be independent" {
-                val stringSaver = NoOpSaver<String>()
-                val intSaver = NoOpSaver<Int>()
-                val listSaver = NoOpSaver<List<String>>()
-
-                // Verify they are different instances (not the same singleton)
-                stringSaver shouldNotBe intSaver
-                stringSaver shouldNotBe listSaver
-                intSaver shouldNotBe listSaver
-            }
-        }
-
-        "when saving state" - {
-            "then save should complete without error" {
-                val saver = NoOpSaver<String>()
-                val testState = "test-state"
-
-                // Should not throw any exception
-                saver.save(testState)
-                saver.save(null)
-            }
-        }
 
         "when restoring state" - {
             "then restore should always return null" {
@@ -41,55 +16,34 @@ class NoOpSaverTest : FreeSpec({
             }
         }
 
-        "when using with different data types" - {
-            "then it should work with any type" {
-                val stringSaver = NoOpSaver<String>()
-                val intSaver = NoOpSaver<Int>()
-                val listSaver = NoOpSaver<List<String>>()
-                val customSaver = NoOpSaver<CustomData>()
-
-                // Test string type
-                stringSaver.save("test")
-                stringSaver.restore() shouldBe null
-
-                // Test int type
-                intSaver.save(42)
-                intSaver.restore() shouldBe null
-
-                // Test list type
-                listSaver.save(listOf("a", "b", "c"))
-                listSaver.restore() shouldBe null
-
-                // Test custom type
-                customSaver.save(CustomData("test", 123))
-                customSaver.restore() shouldBe null
-            }
-        }
-
-        "when saving null state" - {
-            "then save should handle null gracefully" {
+        "when saving various values" - {
+            "then restore should always return null regardless of input" {
                 val saver = NoOpSaver<String>()
 
-                // Should not throw any exception
+                saver.save("value")
+                saver.restore() shouldBe null
+
                 saver.save(null)
                 saver.restore() shouldBe null
             }
         }
 
-        "when used multiple times" - {
-            "then behavior should be consistent" {
-                val saver = NoOpSaver<String>()
+        "when used with different generic types" - {
+            "then restore should return null for each type" {
+                NoOpSaver<Int>().apply {
+                    save(42)
+                    restore() shouldBe null
+                }
 
-                // Multiple saves
-                saver.save("first")
-                saver.save("second")
-                saver.save(null)
-                saver.save("third")
+                NoOpSaver<List<String>>().apply {
+                    save(listOf("a", "b"))
+                    restore() shouldBe null
+                }
 
-                // Multiple restores should always return null
-                saver.restore() shouldBe null
-                saver.restore() shouldBe null
-                saver.restore() shouldBe null
+                NoOpSaver<CustomData>().apply {
+                    save(CustomData("test", 123))
+                    restore() shouldBe null
+                }
             }
         }
     }
