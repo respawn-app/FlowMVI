5ef4a87733abf8c606e4ea0e2f3f1e5b25e7107f
Nek-12 <vaizin.nikita@gmail.com>
2025-11-30 12:26:43 +0100

    chore: enable metrics module

--- metrics/build.gradle.kts
+++ metrics/build.gradle.kts
@@ -1,19 +1,18 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id(libs.plugins.kotlin.multiplatform.id)
     id(libs.plugins.androidLibrary.id)
     alias(libs.plugins.atomicfu)
-    // alias(libs.plugins.maven.publish)
-    // dokkaDocumentation
+    alias(libs.plugins.maven.publish)
+    dokkaDocumentation
 }
 
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     configureMultiplatform(
         ext = this,
-        wasmWasi = false, // datetime does not support wasmWasi
+        wasmWasi = true, // datetime does not support wasmWasi
     )
 }
 
