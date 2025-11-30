68b9c8433be13365c96ecfecb1f11d09000a0c73
Nek-12 <vaizin.nikita@gmail.com>
2025-11-30 17:44:10 +0100

    chore: temporarily set logging level to quite to silence fp warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -176,6 +176,6 @@ gradle.taskGraph.whenReady {
     val lastTask = allTasks.lastOrNull()
     lastTask?.doLast {
         if (this.state.failure != null) return@doLast
-        println("✅ TASK SUCCESSFUL. Some messages suppressed by logging.level=warn")
+        println("✅ TASK SUCCESSFUL. Some messages suppressed by logging.level=quiet")
     }
 }
